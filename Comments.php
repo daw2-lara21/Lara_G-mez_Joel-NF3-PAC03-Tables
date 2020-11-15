73652dd511a50eccf1ce5c3306390f69c7db4b48
Joel Lara <71894655+daw2-lara21@users.noreply.github.com>
2020-11-15 18:53:41 +0100

    Add files via upload

---
+++ Comments.php
@@ -0,0 +1,17 @@
+<?php
+    echo "He aprendido a hacer que pares e impares sean de diferente color mediante un bucle";
+    echo "Hacer un rating";
+    echo "Ordenar comentarios";
+    echo "He aprendido a poner links en PHP";
+    echo "He aprendido a poner detalles";
+    echo "He aprendido a utilizar lo equivalente al select * from en PHP";
+    echo "He aprendido a crear la DB en PHP";
+    echo "He aprendido a utilizar funciones";
+    echo "He aprendido $query con ENDHTML";
+    echo "He aprendido los tr en PHP";
+    echo "La  calificación del documento esta vez le doy un 5, la práctica ha sido complicada y poniéndolo en inglés aún más";
+    echo "La explicación del profesor daré un 9 ya que si no fuese por ella no avanzaba";
+    echo "Como alumno, estoy algo estresado entre tantas prácticas, pero voy haciendo";
+    echo "Ninguna por ahora";
+?>
+
